8e17929ecd815ff19319ab2d0b03613809b8e1d3
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-05-20 22:19:31 +0500

    Installed: axios, final-form & react-final-form, redux, react-redux, react-router-dom, redux-thunk, materialize-css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from "react";
 import './App.css';
+import 'materialize-css'
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      
 
     </div>
   );
