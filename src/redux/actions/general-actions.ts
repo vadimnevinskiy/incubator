1aad48cecd3fe19f7be8800c2da2ea584884859a
Vadim Nevinskiy <vadim_nevinskiy@mail.ru>
2021-06-03 00:25:57 +0500

    REFACTORING: general-actions.ts types renamed

--- src/redux/actions/general-actions.ts
+++ src/redux/actions/general-actions.ts
@@ -1,22 +1,22 @@
 import {FETCHING_TOGGLE, SET_AUTH_ID} from "../vars";
 
-type SetToggleFetchingType = {
+type ToggleFetchingActionType = {
     type: typeof FETCHING_TOGGLE
     fetching: boolean
 }
 
-type SetAuthIdType = {
+type AuthIdActionType = {
     type: typeof SET_AUTH_ID
     authId: number
 }
 
-export const setToggleFetching = (fetching: boolean): SetToggleFetchingType => {
+export const setToggleFetching = (fetching: boolean): ToggleFetchingActionType => {
     return {
         type: FETCHING_TOGGLE,
         fetching: fetching
     }
 }
-export const setAuthId = (authId: number): SetAuthIdType => {
+export const setAuthId = (authId: number): AuthIdActionType => {
     return {
         type: SET_AUTH_ID,
         authId: authId
